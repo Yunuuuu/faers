a5a8eaa93e58969058c0a43ea3b9840f0a99ddaa
yun <yunyunpp96@outlook.com>
2023-10-16 13:01:27 +0800

    don't export allsrc and srclist

--- R/normalize.R
+++ R/normalize.R
@@ -1,30 +1,23 @@
-faers_drug_normalize <- function(terms, exact = TRUE, approximate = TRUE, allsrc = NULL, srclist = NULL, search = 2L) {
+faers_drug_normalize <- function(terms, exact = TRUE, approximate = TRUE, search = 2L) {
     assert_bool(exact)
     assert_bool(approximate)
-    vapply(terms, drug_normalize, character(1L),
+    vapply(terms, drug_normalize_by_network, character(1L),
         exact = exact, approximate = approximate,
-        allsrc = allsrc, srclist = srclist, search = search,
-        USE.NAMES = FALSE
+        search = search, USE.NAMES = FALSE
     )
 }
 
-drug_normalize <- function(term, exact = TRUE, approximate = TRUE, allsrc = NULL, srclist = NULL, search = 2L) {
+drug_normalize_by_network <- function(term, exact = TRUE, approximate = TRUE, allsrc = NULL, srclist = NULL, search = NULL) {
     if (exact) {
         out <- rxnorm_exact_map(term,
             allsrc = allsrc,
             srclist = srclist, search = search
         )
-        out <- xml2::xml_text(xml2::xml_find_first(
-            httr2::resp_body_xml(out),
-            "//rxnormId"
-        ))
+        out <- get_rxnorm_item(out, "//rxnormId")
     }
     if (is.na(out) && approximate) {
         out <- rxnorm_approximate_map(term, max_entries = 1L)
-        out <- xml2::xml_text(xml2::xml_find_first(
-            httr2::resp_body_xml(out),
-            "//rxcui"
-        ))
+        out <- get_rxnorm_item(out, "//rxcui")
     }
     out
 }
