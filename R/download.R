c71ca5266a9a04059eb154e1682003d6630e7749
yun <yunyunpp96@outlook.com>
2024-07-02 17:03:15 +0800

    fix `fda_drugs`: wrong url

--- R/download.R
+++ R/download.R
@@ -8,7 +8,7 @@
 #' @param dir The destination directory for any downloads. Defaults to
 #'  current working dir.
 #' @param ... Extra handle options passed to each request
-#'  [new_handle][curl::new_handle].
+#'  [multi_download][curl::multi_download].
 #' @return An atomic character for the path of downloaded files.
 #' @examples
 #' # you must change `dir`, as the file included in the package is sampled
@@ -34,33 +34,39 @@ faers_download <- function(years, quarters, format = NULL, dir = getwd(), ...) {
     }
     urls <- build_faers_url(format, years, quarters)
     dest_files <- file.path(dir_create2(dir), basename(urls))
-    download_inform(urls, dest_files, handle_opts = list(...))
+    download_inform(urls, dest_files, ...)
 }
 
 #' Download utils function with good message.
 #' @return A character path if downloading successed, otherwise, stop with error
 #'   message.
 #' @noRd
-download_inform <- function(urls, file_paths, handle_opts = list()) {
+download_inform <- function(urls, file_paths, ...) {
     out <- list(
         urls = urls, destfiles = file_paths,
         is_success = rep_len(TRUE, length(urls))
     )
-    is_existed <- file.exists(file_paths)
-    if (any(is_existed)) {
-        cli::cli_inform("Finding {.val {sum(is_existed)}} file{?s} already downloaded: {.file {basename(file_paths[is_existed])}}") # nolint
+    if (any(is_existed <- file.exists(file_paths))) {
+        cli::cli_inform(paste(
+            "Finding {.val {sum(is_existed)}} file{?s} already",
+            "downloaded: {.file {basename(file_paths[is_existed])}}"
+        ))
         urls <- urls[!is_existed]
         file_paths <- file_paths[!is_existed]
     }
-    if (length(urls)) {
+    if (l <- length(urls)) {
         assert_internet()
-        cli::cli_inform("Downloading {.val {length(urls)}} file{?s}")
+        if (l == 1L) {
+            cli::cli_inform("Downloading 1 file from: {.url {urls}}")
+        } else {
+            cli::cli_inform("Downloading {.val {l}} files")
+        }
         arg_list <- c(
             list(
                 urls = urls, destfiles = file_paths, resume = FALSE,
                 progress = interactive(), timeout = Inf
             ),
-            handle_opts
+            rlang::list2(...)
         )
         status <- do.call(curl::multi_download, arg_list)
         is_success <- is_download_success(status)
@@ -73,7 +79,10 @@ download_inform <- function(urls, file_paths, handle_opts = list()) {
             cli::cli_abort(c(
                 "Cannot download {.val {n_failed_files}} file{?s}",
                 "i" = "url{?s}: {.url {urls[!is_success]}}",
-                "!" = "status {cli::qty(n_failed_files)} code{?s}: {.val {status$status_code[!is_success]}}",
+                "!" = paste(
+                    "status {cli::qty(n_failed_files)} code{?s}:",
+                    "{.val {status[!is_success]}}"
+                ),
                 x = "error {cli::qty(n_failed_files)} message{?s}: {.val {status$error[!is_success]}}"
             ))
         }
@@ -86,6 +95,44 @@ download_inform <- function(urls, file_paths, handle_opts = list()) {
 is_download_success <- function(status, successful_code = c(200L, 206L, 416L)) {
     status$success & !is.na(status$success) &
         status$status_code %in% successful_code
+}
+
+base_download_inform <- function(urls, file_paths, ...) {
+    out <- file_paths
+    if (any(is_existed <- file.exists(file_paths))) {
+        cli::cli_inform(paste(
+            "Finding {.val {sum(is_existed)}} file{?s} already",
+            "downloaded: {.file {basename(file_paths[is_existed])}}"
+        )) # nolint
+        urls <- urls[!is_existed]
+        file_paths <- file_paths[!is_existed]
+    }
+    if (l <- length(urls)) {
+        assert_internet()
+        if (l == 1L) {
+            cli::cli_inform("Downloading 1 file from: {.url {urls}}")
+        } else {
+            cli::cli_inform("Downloading {.val {l}} files")
+        }
+        status <- utils::download.file(urls,
+            destfile = file_paths, ..., method = "libcurl"
+        )
+        is_success <- status == 0L
+        is_need_deleted <- !is_success & file.exists(file_paths)
+        if (any(is_need_deleted)) file.remove(file_paths[is_need_deleted])
+        if (!all(is_success)) {
+            n_failed_files <- sum(!is_success) # nolint
+            cli::cli_abort(c(
+                "Cannot download {.val {n_failed_files}} file{?s}",
+                "i" = "url{?s}: {.url {urls[!is_success]}}",
+                "!" = paste(
+                    "status {cli::qty(n_failed_files)} code{?s}:",
+                    "{.val {status[!is_success]}}"
+                )
+            ))
+        }
+    }
+    out
 }
 
 build_faers_url <- function(type, years, quarters) {
